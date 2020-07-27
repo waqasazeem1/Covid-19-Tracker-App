24219afee5311b9246175b37d081e197d1121700
waqas azeem <waqasazeem985@gmail.com>
2020-07-27 23:31:05 +0500

    get countries data
    get countries data from api and show the data in dropdown

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MenuItem, Select, FormControl } from "@material-ui/core";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          // set in state
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
       <div className="app_header">
         <h1>Covid-19 App</h1>
         <FormControl className="app_dropdown">
           <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+            {/* <MenuItem value="worldwide">worldwide</MenuItem>
             <MenuItem value="worldwide">worldwide</MenuItem>
-            <MenuItem value="worldwide">worldwide</MenuItem>
-            <MenuItem value="worldwide">worldwide</MenuItem>
-            <MenuItem value="worldwide">worldwide</MenuItem>
+            <MenuItem value="worldwide">worldwide</MenuItem> */}
           </Select>
         </FormControl>
       </div>
