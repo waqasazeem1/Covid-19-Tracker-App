0d6206be3a1745b5d8e3599e76714a4b6212f1ce
waqas azeem <waqasazeem985@gmail.com>
2020-07-29 17:53:20 +0500

    use ternart operator for sort country data

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,17 @@
 export const sortData = (data) => {
   const sortedData = [...data];
 
-  sortedData.sort((a, b) => {
-    if (a.cases > b.cases) {
-      return -1;
-    } else {
-      return 1;
-    }
-  });
-  return sortedData;
+  //   ternary operator
+  return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+
+  // OR sendcom=n way
+
+  //   sortedData.sort((a, b) => {
+  //     if (a.cases > b.cases) {
+  //       return -1;
+  //     } else {
+  //       return 1;
+  //     }
+  //   });
+  //   return sortedData;
 };
