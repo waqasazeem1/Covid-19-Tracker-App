a902862cac4213a6f336cdbb8e07b8192c510073
waqas azeem <waqasazeem985@gmail.com>
2020-07-30 20:06:04 +0500

    show country case in "," format

--- src/CountryTable.js
+++ src/CountryTable.js
@@ -1,4 +1,5 @@
 import React from "react";
+import numeral from "numeral";
 import "./Table.css";
 
 function CountryTable({ countries }) {
@@ -9,7 +10,7 @@ function CountryTable({ countries }) {
         <tr>
           <td>{country}</td>
           <td>
-            <strong>{cases}</strong>
+            <strong>{numeral(cases).format(0, 0)}</strong>
           </td>
         </tr>
       ))}
